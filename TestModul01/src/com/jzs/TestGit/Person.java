17d574e256992b2b4e9751ecd01026bc4b7da9f7
jzs <1660211953@qq.com>
2022-05-26 21:21:29 +0800

    在person中添加了年龄

---
+++ TestModul01/src/com/jzs/TestGit/Person.java
@@ -0,0 +1,11 @@
+package com.jzs.TestGit;
+
+/**
+ * @Auther: jzs
+ * @Date: 2022/5/26 - 05 - 26 - 21:20
+ * @Description: com.jzs.TestGit
+ * @version: 1.0
+ */
+public class Person {
+    private  int age;
+}
